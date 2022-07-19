7fe243d2be61bae8a5776bb6bf4e7a3f23b18ca4
AlnKrvchnk <alinakrvchnk@gmail.com>
2022-07-19 10:36:59 +0300

    add storybook in gitignor

--- my-app/src/routes/Paths.ts
+++ my-app/src/routes/Paths.ts
@@ -3,7 +3,7 @@ import {Link} from '../types/Link'
 export enum Paths {
     Main = '/Traineeship-Utouch/',
     SignIn = '/Traineeship-Utouch/signin',
-    SignUp = '/Traineeship-Utouch/signUp',
+    SignUp = '/Traineeship-Utouch/signup',
     ToDo='/Traineeship-Utouch/todo'
 }
 
