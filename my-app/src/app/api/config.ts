6544e8d6624c27ab19955671f470b5d0661b8e8a
AlnKrvchnk <alinakrvchnk@gmail.com>
2022-07-19 08:33:42 +0300

    add Store, styledComponents

--- my-app/src/app/api/config.ts
+++ my-app/src/app/api/config.ts
@@ -1,6 +1,6 @@
 import {AxiosRequestConfig} from 'axios'
 
 export const config: AxiosRequestConfig = {
-    baseURL: 'http://www.filltext.com/?rows=10&id={index}&email={email}&username={username}&password={randomString|5}&pretty=true',
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true
 }
