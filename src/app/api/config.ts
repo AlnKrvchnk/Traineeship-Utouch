8a14a6c1d9ea3f39981d0cc1b4acc0d017a15fd0
AlnKrvchnk <alinakrvchnk@gmail.com>
2022-07-29 16:11:00 +0300

    add dotenv-webpack

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,8 +1,7 @@
-import { AxiosRequestConfig } from "axios";
-const process = require("process");
+import { AxiosRequestConfig } from 'axios';
 
 export const config: AxiosRequestConfig = {
-  baseURL: "https://mockend.com/AlnKrvchnk/Mockend",
-  //   baseURL: process.env.REACT_APP_API_URL,
-  // withCredentials: true
+    // baseURL: 'https://mockend.com/AlnKrvchnk/Mockend',
+    baseURL: process.env.REACT_APP_API_URL,
+    // withCredentials: true
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,50 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-const production = process.env.NODE_ENV === "production";
+const production = process.env.NODE_ENV === 'production';
 
 const config = {
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: "ts-loader",
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+        new ESLintPlugin(),
+        new Dotenv(),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new ESLintPlugin(),
-  ],
-  devServer: {
-    port: 3001,
-    hot: true,
-  },
-  mode: production ? "production" : "development",
+    devServer: {
+        port: 3001,
+        hot: true,
+    },
+    mode: production ? 'production' : 'development',
 };
 module.exports = config;
