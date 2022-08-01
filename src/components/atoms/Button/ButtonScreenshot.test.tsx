4dd6c17cf9aa4fff10762f5c58ea0b9d8c2e998f
AlnKrvchnk <alinakrvchnk@gmail.com>
2022-08-01 13:36:51 +0300

    add screenshot test

---
+++ src/components/atoms/Button/ButtonScreenshot.test.tsx
@@ -0,0 +1,7 @@
+import renderer from 'react-test-renderer';
+import Button from './Button';
+
+it('renders correctly', () => {
+    const button = renderer.create(<Button small>Button</Button>).toJSON();
+    expect(button).toMatchSnapshot();
+});
